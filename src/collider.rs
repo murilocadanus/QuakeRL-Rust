7b36b5bb605ef136e21806a6f5188340a98f5038
Danny Angelo Carminati Grein <fungos@gmail.com>
2015-01-03 00:57:29 -0500

    Add test to verify if an AABB contains another. Add some documentation and test cases.

--- src/collider.rs
+++ src/collider.rs
@@ -1,12 +1,14 @@
 extern crate graphics;
-
 use piston::graphics::*;
 use render::{Render, Draw};
+
 use std::num::{Float, FloatMath};
 
 pub struct AABB {
-    pub center: [f64, ..2],
-    pub half_size: [f64, ..2],
+    center: [f64, ..2],
+    half_size: [f64, ..2],
+    lower_corner: [f64, ..2],
+    upper_corner: [f64, ..2],
 }
 
 impl AABB {
@@ -38,43 +40,66 @@ impl AABB {
         AABB {
             center: c,
             half_size: r,
+            lower_corner: [c[0] - r[0], c[1] - r[1]],
+            upper_corner: [c[0] + r[0], c[1] + r[1]],
         }
     }
 
     pub fn set_pos(&mut self, pos: [f64, ..2]) {
         self.center = pos;
+
+        let (lb, ub) = self.get_corners();
+        self.lower_corner = lb;
+        self.upper_corner = ub;
+    }
+
+    pub fn get_corners(&self) -> ([f64, ..2], [f64, ..2]) {
+        (
+            [self.center[0] - self.half_size[0], self.center[1] - self.half_size[1]],
+            [self.center[0] + self.half_size[0], self.center[1] + self.half_size[1]],
+        )
+    }
+
+    #[allow(dead_code)]
+    pub fn get_dimension(&self) -> [f64, ..2] {
+        [self.half_size[0] * 2.0, self.half_size[1] * 2.0]
+    }
+
+    #[allow(dead_code)]
+    pub fn contains_aabb(&self, other: &AABB) -> bool {
+        //! Find if this AABB fully contains another AABB.
+        //! This is an exact corner comparation, so it may have some float imprecision.
+        other.lower_corner[0] >= self.lower_corner[0] &&
+        other.upper_corner[0] <= self.upper_corner[0] &&
+        other.lower_corner[1] >= self.lower_corner[1] &&
+        other.upper_corner[1] <= self.upper_corner[1]
     }
 
     pub fn intersect_aabb(&self, other: &AABB) -> bool {
-        // this calculates the area overlaped. we must compare performance here, but this one
-        // fixes the imprecision on collision compared with the boolean one.
+        //! Finds if there is an intersection between two AABB.
+        //! This add a little rounding factor so we can accept a very tiny overlap area.
+        // this fixes the imprecision on collision compared with the contains_aabb.
         let x = (self.right().min(other.right())   - self.left().max(other.left())).max(0.0);
         let y = (self.bottom().min(other.bottom()) - self.top().max(other.top())  ).max(0.0);
         x * y > 0.0
-/*
-        self.right() >= other.left() &&
-        self.left() <= other.right() &&
-        self.top() <= other.bottom() &&
-        self.bottom() >= other.top()
-*/
     }
 
     // we add .4 and do a floor so we can round to the near int on both sides
     // (left and right or up and down) correctly
     fn left(&self) -> f64 {
-        (self.center[0] - self.half_size[0] + 0.4).floor()
+        (self.lower_corner[0] + 0.4).floor()
     }
 
     fn right(&self) -> f64 {
-        (self.center[0] + self.half_size[0] + 0.4).floor()
+        (self.upper_corner[0] + 0.4).floor()
     }
 
     fn top(&self) -> f64 {
-        (self.center[1] - self.half_size[1] + 0.4).floor()
+        (self.lower_corner[1] + 0.4).floor()
     }
 
     fn bottom(&self) -> f64 {
-        (self.center[1] + self.half_size[1] + 0.4).floor()
+        (self.upper_corner[1] + 0.4).floor()
     }
 }
 
@@ -94,5 +119,71 @@ impl Draw for AABB {
                 [-w, -h, w * 2.0, h * 2.0], collider_ctx, &mut render.gl
             );
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn intersect_aabb()
+    {
+        //! Test these intersections
+        //!
+        //! +-----+     +-----+
+        //! | 1   |     | 2   |
+        //! |  +-----------+  |
+        //! |  |  |     |  |  |
+        //! +--|--+  3  +--|--+
+        //!    +-----------+
+        //!
+        let box1 = AABB::new([10.0, 10.0], [5.0, 5.0]);
+        assert_eq!(box1.get_dimension(), [10.0, 10.0]);
+
+        let box2 = AABB::new([30.0, 10.0], [5.0, 5.0]);
+        assert_eq!(box1.intersect_aabb(&box2), false);
+        assert_eq!(box2.intersect_aabb(&box1), false);
+
+        let mut box3 = AABB::new([0.0, 0.0], [10.0, 5.0]);
+        assert_eq!(box3.get_dimension(), [20.0, 10.0]);
+
+        box3.set_pos([20.0, 15.0]);
+        assert_eq!(box1.intersect_aabb(&box3), true);
+        assert_eq!(box2.intersect_aabb(&box3), true);
+        assert_eq!(box3.intersect_aabb(&box1), true);
+        assert_eq!(box3.intersect_aabb(&box2), true);
+    }
+
+    #[test]
+    fn contains_aabb()
+    {
+        //! Test these intersections
+        //!
+        //! +-----------------+
+        //! | 1               |
+        //! |  +-----------+  |
+        //! |  | 2         |  |
+        //! |  |           |  |
+        //! |  +-----------+  |
+        //! +-----------------+
+        //!
+        let box1 = AABB::new([55.0, 55.0], [50.0, 50.0]);
+        assert_eq!(box1.get_dimension(), [100.0, 100.0]);
+        assert_eq!(box1.get_corners(), ([5.0, 5.0], [105.0, 105.0]));
+
+        let mut box2 = AABB::new([0.0, 0.0], [10.0, 10.0]);
+        assert_eq!(box2.get_corners(), ([-10.0, -10.0], [10.0, 10.0]));
+
+        assert_eq!(box2.contains_aabb(&box1), false);
+        assert_eq!(box1.contains_aabb(&box2), false);
+        assert_eq!(box1.intersect_aabb(&box2), true);
+        assert_eq!(box2.intersect_aabb(&box1), true);
+
+        box2.set_pos([45.0, 45.0]);
+        assert_eq!(box1.contains_aabb(&box2), true);
+        assert_eq!(box2.contains_aabb(&box1), false);
+        assert_eq!(box1.intersect_aabb(&box2), true);
+        assert_eq!(box2.intersect_aabb(&box1), true);
     }
 }
