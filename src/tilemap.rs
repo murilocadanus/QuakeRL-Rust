576ab26ea913477941e47e05bf7da54e91297977
Marlus Cadanus da Costa <marluscadanus@gmail.com>
2015-01-03 17:00:28 -0200

    FIX: Changed from current to quack to render the tilemap

--- src/tilemap.rs
+++ src/tilemap.rs
@@ -1,10 +1,10 @@
 extern crate graphics;
 
-//use render::{Render, Draw};
+use render::{Render, Draw};
 use sprite::Sprite;
 use std::vec::Vec;
 use piston::graphics::*;
-//use current::Set;
+use quack::Set;
 
 #[allow(dead_code)]
 enum Tiles {
@@ -82,7 +82,6 @@ impl TileMap {
 
 }
 
-/*
 impl Draw for TileMap {
     fn draw(&self, render: &mut Render) {
 
@@ -94,7 +93,7 @@ impl Draw for TileMap {
             }
         }
 
+
     }
 }
-*/
 
